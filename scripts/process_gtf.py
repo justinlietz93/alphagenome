ba3c371479ea6afe730b67b9c2c9bc5f031f816f
Tom Ward <tomward@google.com>
2026-01-14 03:47:11 -0800

    Add support for processing splice sites from GTF.
    PiperOrigin-RevId: 856141753
    Change-Id: I30a884451e257d7e5a526e6bf69fbb7f23873c04

--- scripts/process_gtf.py
+++ scripts/process_gtf.py
@@ -22,6 +22,8 @@ from urllib import request
 from absl import app
 from absl import flags
 from absl import logging
+from alphagenome.data import transcript as transcript_utils
+import pandas as pd
 import pyranges
 
 
@@ -29,28 +31,82 @@ _GTF_PATH = flags.DEFINE_string(
     'gtf_path', None, 'Path to GTF file.', required=True
 )
 _OUTPUT_PATH = flags.DEFINE_string(
-    'output_path', None, 'Path to output feather file.', required=True
+    'output_path', None, 'Path to GTF output feather file.', required=True
+)
+_SPLICE_SITES_OUTPUT_PATH = flags.DEFINE_string(
+    'splice_sites_output_path',
+    None,
+    'Path to splice_sites output feather file.',
 )
 
 
-def main(_) -> None:
-  logging.info('Reading GTF from %s', _GTF_PATH.value)
-  url = parse.urlparse(_GTF_PATH.value)
+def generate_splice_sites(
+    gtf: pd.DataFrame,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+  """Generates tissue-agnostic splice start/end sites from GTF."""
+  transcripts = {
+      k: transcript_utils.Transcript.from_gtf_df(df)
+      for k, df in gtf.groupby('transcript_id')
+  }
+  junctions = []
+  for _, transcript in transcripts.items():
+    junctions.extend(
+        [x.chromosome, x.start, x.end, x.strand] for x in transcript.introns
+    )
+  df = pd.DataFrame(
+      junctions, columns=['Chromosome', 'Start', 'End', 'Strand']
+  ).sort_values(by=['Chromosome', 'Start'])
+  df = df.drop_duplicates().reset_index(drop=True)
+  return (
+      df[['Chromosome', 'Start', 'Strand']],
+      df[['Chromosome', 'End', 'Strand']],
+  )
+
+
+def generate_gtf(path: str) -> pd.DataFrame:
+  """Generates GTF from a GTF path."""
+  url = parse.urlparse(path)
   if all([url.scheme, url.netloc]):
     with tempfile.TemporaryDirectory() as d:
       path, _ = request.urlretrieve(
-          _GTF_PATH.value,
-          filename=os.path.join(d, os.path.basename(_GTF_PATH.value)),
+          path,
+          filename=os.path.join(d, os.path.basename(path)),
       )
       logging.info('Downloaded GTF to %s', path)
       gtf = pyranges.read_gtf(path, as_df=True, duplicate_attr=True)
   else:
-    gtf = pyranges.read_gtf(_GTF_PATH.value, as_df=True, duplicate_attr=True)
+    gtf = pyranges.read_gtf(path, as_df=True, duplicate_attr=True)
 
   gtf['gene_id_nopatch'] = gtf['gene_id'].str.split('.', expand=True)[0]
+  return gtf
+
+
+def main(_) -> None:
+  if not _OUTPUT_PATH.value.endswith('.feather'):
+    raise ValueError('Output path must end with .feather')
+
+  logging.info('Reading GTF from %s', _GTF_PATH.value)
+  gtf = generate_gtf(_GTF_PATH.value)
 
   logging.info('Writing GTF to %s', _OUTPUT_PATH.value)
   gtf.to_feather(_OUTPUT_PATH.value)
+
+  if _SPLICE_SITES_OUTPUT_PATH.value is not None:
+    logging.info('Generating splice sites from GTF')
+    splice_sites_starts, splice_sites_ends = generate_splice_sites(gtf)
+
+    splice_sites_start_path = (
+        _SPLICE_SITES_OUTPUT_PATH.value.removesuffix('.feather')
+        + '_starts.feather'
+    )
+    splice_sites_end_path = (
+        _SPLICE_SITES_OUTPUT_PATH.value.removesuffix('.feather')
+        + '_ends.feather'
+    )
+    logging.info('Writing start splice sites to %s', splice_sites_start_path)
+    splice_sites_starts.to_feather(splice_sites_start_path)
+    logging.info('Writing end splice sites to %s', splice_sites_end_path)
+    splice_sites_ends.to_feather(splice_sites_end_path)
 
 
 if __name__ == '__main__':
